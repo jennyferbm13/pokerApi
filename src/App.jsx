89d154f08c143624e171722c736b27b7615dfe88
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-04-19 11:46:06 -0500

    Se logra hacer peticion, falta organizar

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let random = Math.floor(Math.random() * 100) + 1; // Genera un número aleatorio entre 1 y 100
+  let url = `https://pokeapi.co/api/v2/pokemon/${random}`; // Construye la URL de la petición
+
+  const [pokemon, setPokemon] = useState();
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemon(data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {pokemon && <p>{pokemon.name}</p>}
+      {pokemon && <img src={pokemon.sprites.front_default}></img>}
+      {pokemon &&
+        pokemon.types.map((type) => <li key={pokemon.id}>{type.type.name}</li>)}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
