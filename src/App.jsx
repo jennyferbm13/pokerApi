1e11fa5956ecd64272d0da47037a478b1274a474
jennyferbm13 <jennyfer.belalcazar@correounivalle.edu.co>
2023-04-19 12:06:19 -0500

    Agrego estilps y boton

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,47 @@ function App() {
   const [pokemon, setPokemon] = useState();
 
   useEffect(() => {
+    refresh();
+  }, []);
+
+  function refresh() {
+    random = Math.floor(Math.random() * 100) + 1;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
         setPokemon(data);
       })
       .catch((error) => console.error(error));
-  }, []);
+  }
 
   return (
     <div className="App">
-      {pokemon && <p>{pokemon.name}</p>}
-      {pokemon && <img src={pokemon.sprites.front_default}></img>}
-      {pokemon &&
-        pokemon.types.map((type) => <li key={pokemon.id}>{type.type.name}</li>)}
+      <div className="container">
+        <div className="ladoIzquierdo">
+          {pokemon && (
+            <img
+              src={pokemon.sprites.front_default}
+              alt="Imagen de un pokemon aletorio sacado de la API https://pokeapi.co/api/v2/pokemon/{random}"
+            ></img>
+          )}
+        </div>
+
+        <div className="ladoDerecho">
+          {pokemon && <p>{pokemon.name}</p>}
+          {pokemon &&
+            pokemon.types.map((type) => (
+              <li key={pokemon.id}>{type.type.name}</li>
+            ))}
+        </div>
+      </div>
+      <button
+        className="refresh"
+        onClick={() => {
+          refresh();
+        }}
+      >
+        Refrescar
+      </button>
     </div>
   );
 }
